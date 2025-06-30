46a7571f651bcb1db3d798781ba926e449b8cb63
anarorujov <anarorujov26@gmail.com>
2025-06-30 19:20:54 +0400

    asd

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ dotenv.config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || '127.0.0.1',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || 'Anar29092006',
   database: process.env.DB_NAME || 'craftnet',
   waitForConnections: true,
   connectionLimit: 10,
