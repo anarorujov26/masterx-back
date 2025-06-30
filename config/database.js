35134390da5df1907cda69a4756431a0f59ec575
anarorujov <anarorujov26@gmail.com>
2025-06-30 15:55:11 +0400

    'init'

--- config/database.js
+++ config/database.js
@@ -3,17 +3,16 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-// MySQL connection pool yaradırıq
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.127.126.3',
-  user: process.env.DB_USER || 'root',
-  port: 3306,
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'craftnet',
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'craftnet',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+
 
 // Promise wrapper ilə pool'u export edirik
 module.exports = pool.promise(); 
